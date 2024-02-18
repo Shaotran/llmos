4555dd82e08bb31b3cff0d3d969c04984204c194
Ethan Shaotran <eshaotran@college.harvard.edu>
2024-02-17 16:36:13 -0800

    :zap: (llmosapp): Full working demo

--- llmosapp/llmos/ContentView.swift
+++ llmosapp/llmos/ContentView.swift
@@ -27,25 +27,81 @@ struct ContentView: View {
     private let expandedSize: CGFloat = 250
     @State private var audioFile: AVAudioFile?
     @State private var outputFileURL: URL?
+    @State private var animate = false
+    @State var isRotating = false
     
     @State private var status = TranscriptionState.waiting
     
     let helpers = Helpers()
     
     var body: some View {
-        Circle()
-            .fill(Color.blue)
-            .frame(width: isRecording ? expandedSize : normalSize, height: isRecording ? expandedSize : normalSize)
-            .shadow(radius: 10)
-            .animation(.easeInOut(duration: 0.5), value: isRecording)
-            .onTapGesture {
-                if isRecording {
-                    stopRecording()
-                } else {
-                    startRecording()
-                }
-                isRecording.toggle()
+        ZStack {
+            ZStack {
+                Image("shadow")
+                Image("icon-bg")
+                Image("pink-top")
+                    .rotationEffect(.degrees(isRotating ? 320 : -360))
+                    .hueRotation(.degrees(isRotating ? -270 : 60))
+                
+                Image("pink-left")
+                    .rotationEffect(.degrees(isRotating ? -360 : 180))
+                    .hueRotation(.degrees(isRotating ? -220 : 300))
+                
+                Image("blue-middle")
+                    .rotationEffect(.degrees(isRotating ? -360 : 420))
+                    .hueRotation(.degrees(isRotating ? -150 : 0))
+                    .rotation3DEffect(.degrees(75), axis: (x: isRotating ? 1 : 5, y: 0, z: 0))
+                
+                Image("blue-right")
+                    .rotationEffect(.degrees(isRotating ? -360 : 420))
+                    .hueRotation(.degrees(isRotating ? 720 : -50))
+                    .rotation3DEffect(.degrees(75), axis: (x: 1, y: 0, z: isRotating ? -5 : 15))
+                
+                Image("intersect")
+                    .rotationEffect(.degrees(isRotating ? 30 : -420))
+                    .hueRotation(.degrees(isRotating ? 0 : 720))
+                    .rotation3DEffect(.degrees(15), axis: (x: 1, y: 1, z: 1), perspective: isRotating ? 5 : -5)
+                
+                Image("green-right")
+                    .rotationEffect(.degrees(isRotating ? -300 : 360))
+                    .hueRotation(.degrees(isRotating ? 300 : -15))
+                    .rotation3DEffect(.degrees(15), axis: (x: 1, y: isRotating ? -1 : 1, z: 0), perspective: isRotating ? -1 : 1)
+                
+                Image("green-left")
+                    .rotationEffect(.degrees(isRotating ? 360 : -360))
+                    .hueRotation(.degrees(isRotating ? 180 :50))
+                    .rotation3DEffect(.degrees(75), axis: (x: 1, y:isRotating ? -5 : 15, z: 0))
+                
+                
+                Image("bottom-pink")
+                    .rotationEffect(.degrees(isRotating ? 400 : -360))
+                    .hueRotation(.degrees(isRotating ? 0 : 230))
+                    .opacity(0.25)
+                    .blendMode(.multiply)
+                    .rotation3DEffect(.degrees(75), axis: (x: 5, y:isRotating ? 1 : -45, z: 0))
             }
+            .blendMode(isRotating ? .hardLight : .difference )
+            .scaleEffect(isRecording ? 1.1 : 1.0) // Adjust scale effect based on recording state
+            .animation(isRecording ? Animation.easeInOut(duration: 1).repeatForever(autoreverses: true) : .default, value: isRecording) // Apply pulsing animation only when recording
+            
+            Image("highlight")
+                .rotationEffect(.degrees(isRotating ? 360 : 250))
+                .hueRotation(.degrees(isRotating ? 0 : 230))
+                .padding()
+                .onAppear{
+                    withAnimation(.linear(duration: 12).repeatForever(autoreverses: true)) {
+                        isRotating.toggle()
+                    }
+                }
+        }.scaleEffect(0.6)
+        .onTapGesture {
+            if isRecording {
+                stopRecording()
+            } else {
+                startRecording()
+            }
+            isRecording.toggle()
+        }
     }
 
     func startRecording() {
@@ -131,7 +187,12 @@ struct ContentView: View {
                 let convertedUrl = try await helpers.convertCafToM4a(sourceUrl: sourceUrl)
                 let command = try await helpers.voicetotext(fileUrl: convertedUrl)
                 print(command)
-//                let code = try await helpers.texttocode(command: command)
+                
+                let code = try await helpers.texttocode(command: command)
+                print("Last thing:")
+                print(code)
+                
+                try await helpers.executeAppleScript(code: code)
             } catch {
                 // Handle any errors that occurred during conversion or processing
                 print("An error occurred: \(error)")
--- llmosapp/llmos/helpers.swift
+++ llmosapp/llmos/helpers.swift
@@ -10,6 +10,7 @@ import AVFoundation
 import SwiftWhisper
 import AudioKit
 import OpenAI
+import AppKit
 
 struct Helpers {
     func voicetotext(fileUrl: URL) async throws -> String {
@@ -37,7 +38,7 @@ struct Helpers {
         return result.text
     }
     
-    func texttocode(command: String) async throws -> Void {
+    func texttocode(command: String) async throws -> String {
         var apiKey = ""
         do {
             apiKey = try getAPIKey()
@@ -49,14 +50,69 @@ struct Helpers {
         let openAI = OpenAI(apiToken: apiKey)
                 
         let query = ChatQuery(model: .gpt4_turbo_preview, messages: [
-            .init(role: .system, content: "You are Librarian-GPT. You know everything about the books."),
-            .init(role: .user, content: "Who wrote Harry Potter?")
+            .init(role: .system, content: """
+                You are a helpful personal assistant.
+            """),
+            .init(role: .user, content: Prompt.promptStr + command)
         ])
         
+        print("Will now query for code")
+                
         let result = try await openAI.chats(query: query)
+        print("Got the code")
         print(result)
-        
-        
+        var extractedCode = ""
+        if let content = result.choices[0].message.content {
+            extractedCode = extractCode(from: content)[0]
+        } else {
+            print("Content is nil")
+        }
+        print("Extracted")
+        return extractedCode
+    }
+    
+    func extractCode(from text: String) -> [String] {
+        do {
+            let regex = try NSRegularExpression(pattern: "```(?:applescript)?(.+?)```", options: [.dotMatchesLineSeparators])
+            let nsString = text as NSString
+            let results = regex.matches(in: text, options: [], range: NSRange(location: 0, length: nsString.length))
+            return results.map { nsString.substring(with: $0.range(at: 1)) }
+        } catch let error {
+            print("Invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
+    
+    func executeAppleScript(code: String) {
+//        var error: NSDictionary?
+//        if let script = NSAppleScript(source: code) {
+//            let output = script.executeAndReturnError(&error)
+//            if let error = error {
+//                print(error)
+//            } else {
+//                print(output)
+//            }
+//        }
+        print("LETS GO")
+        let process = Process()
+        let pipe = Pipe()
+
+        process.executableURL = URL(fileURLWithPath: "/usr/bin/osascript")
+        process.arguments = ["-e", code]
+        process.standardOutput = pipe
+        process.standardError = pipe
+
+        do {
+            try process.run()
+            process.waitUntilExit()
+
+            let data = pipe.fileHandleForReading.readDataToEndOfFile()
+            if let output = String(data: data, encoding: .utf8) {
+                print(output)
+            }
+        } catch {
+            print("Failed to execute script: \(error)")
+        }
     }
     
     func convertCafToM4a(sourceUrl: URL) async throws -> URL {
